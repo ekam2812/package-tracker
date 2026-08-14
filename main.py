c41ca224ea1c48db2a72f431c6587973630e8b12
Ekamvir Singh Mangat <ekamvir@Ekamvirs-MacBook-Air.local>
2026-08-13 17:15:18 -0700

    Milestone 3: form submits tracking number to /intake route

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 
 
 app = FastAPI()
 
+# tell Jinja2 which folder to look in
 templates = Jinja2Templates(directory= "templates")
 
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(request, "index.html")
+
+@app.post("/intake")
+def intake(tracking_number: str = Form(...)):
+    return {"tracking number": tracking_number}
