b295d7fd8641251c53213d6c42a832daea12363a
Ekamvir Singh Mangat <ekamvir@Ekamvirs-MacBook-Air.local>
2026-08-16 17:12:24 -0700

    Milestone 4: intake route writes packages to SQLite database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-
+from datetime import datetime
+import sqlite3
 
 
 app = FastAPI()
@@ -14,4 +15,14 @@ def home(request: Request):
 
 @app.post("/intake")
 def intake(tracking_number: str = Form(...)):
+    date_logged = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
+    connection = sqlite3.connect("packages.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO packages (tracking_number, date_logged, status) VALUES (?, ?, ?)", 
+        (tracking_number, date_logged, "pending")
+    )
+    connection.commit()
+
     return {"tracking number": tracking_number}
